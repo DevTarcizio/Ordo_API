1ff4f697b969c137aafca5180607dc707a81a4a4
DevTarcizio <159829865+DevTarcizio@users.noreply.github.com>
2026-01-05 18:23:44 -0300

    Crud feito - aula 3 concluida

--- ordo_fast/app.py
+++ ordo_fast/app.py
@@ -1,11 +1,11 @@
 from http import HTTPStatus
 
-from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, HTTPException
 
-from ordo_fast.schemas import Message
+from ordo_fast.schemas import Message, UserDB, UserList, UserPublic, UserSchema
 
-app = FastAPI()
+app = FastAPI(title='Ordo Praesidium')
+database = []
 
 
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
@@ -13,14 +13,62 @@ def read_root():
     return {'message': 'olá mundo'}
 
 
-@app.get('/html', status_code=HTTPStatus.OK, response_class=HTMLResponse)
-def read_html():
-    return """
-    <html>
-      <head>
-        <title> Nosso olá mundo!</title>
-      </head>
-      <body>
-        <p>Olá Mundo!</p>
-      </body>
-    </html>"""
+@app.get('/users/', status_code=HTTPStatus.OK, response_model=UserList)
+def read_users():
+    return {'users': database}
+
+
+@app.get('/users/{user_id}', status_code=HTTPStatus.OK, response_model=UserPublic)
+def read_user(user_id: int):
+    if user_id < 1 or user_id > len(database):
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='ID not found'
+        )
+
+    user_read = database[user_id - 1]
+
+    return user_read
+
+
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    user_with_id = UserDB(
+        username=user.username,
+        email=user.email,
+        password=user.password,
+        id=len(database) + 1,
+    )
+
+    database.append(user_with_id)
+
+    return user_with_id
+
+
+@app.put('/users/{user_id}', status_code=HTTPStatus.OK, response_model=UserPublic)
+def update_user(user_id: int, user: UserSchema):
+    if user_id < 1 or user_id > len(database):
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='ID not found'
+        )
+
+    user_with_id = UserDB(
+        username=user.username,
+        email=user.email,
+        password=user.password,
+        id=user_id,
+    )
+
+    database[user_id - 1] = user_with_id
+
+    return user_with_id
+
+
+@app.delete(
+    '/users/{user_id}', status_code=HTTPStatus.OK, response_model=UserPublic
+)
+def delete_user(user_id: int):
+    if user_id < 1 or user_id > len(database):
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='ID not found'
+        )
+    return database.pop(user_id - 1)
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,12 +1,7 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from ordo_fast.app import app
-
-
-def test_read_root_retorna_ok_e_ola_mundo():
-    client = TestClient(app)
+def test_read_root_retorna_ok_e_ola_mundo(client):
 
     response = client.get('/')
 
@@ -14,10 +9,97 @@ def test_read_root_retorna_ok_e_ola_mundo():
     assert response.json() == {'message': 'olá mundo'}
 
 
-def test_read_html_retorna_ok_e_ola_mundo():
-    client = TestClient(app)
+def test_create_user(client):
 
-    response = client.get('/html')
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'alice',
+            'email': 'alice@example.com',
+            'password': 'secret',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'username': 'alice',
+        'email': 'alice@example.com',
+        'id': 1,
+    }
+
+
+def test_read_user(client):
+    response = client.get('/users/')
 
     assert response.status_code == HTTPStatus.OK
-    assert "Olá Mundo!" in response.text
+    assert response.json() == {
+        'users': [{'id': 1, 'email': 'alice@example.com', 'username': 'alice'}]
+    }
+
+
+def test_read_unique_user(client):
+    response = client.get('/users/1')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'id': 1,
+        'email': 'alice@example.com',
+        'username': 'alice',
+    }
+
+
+def test_read_unique_user_error(client):
+    response = client.get('/users/2')
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'ID not found'}
+
+
+def test_update_user(client):
+    response = client.put(
+        '/users/1',
+        json={
+            'username': 'bob',
+            'email': 'bob@example.com',
+            'password': 'secret',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'username': 'bob',
+        'email': 'bob@example.com',
+        'id': 1,
+    }
+
+
+def test_update_user_error(client):
+    response = client.put(
+        '/users/2',
+        json={
+            'username': 'bob',
+            'email': 'bob@example.com',
+            'password': 'secret',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'ID not found'}
+
+
+def test_delete_user(client):
+    response = client.delete('/users/1')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'username': 'bob',
+        'email': 'bob@example.com',
+        'id': 1,
+    }
+
+
+def test_delete_user_error(client):
+    response = client.delete('/users/2')
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'ID not found'}
