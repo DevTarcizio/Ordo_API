c3392bfbdd5845532e1911eb262f613ff5a6fd29
DevTarcizio <159829865+DevTarcizio@users.noreply.github.com>
2026-01-06 01:31:08 -0300

    Aula 4 - Criação do DB e configuração do migrations

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,45 @@
+from contextlib import contextmanager
+from datetime import datetime
+
 import pytest
 from fastapi.testclient import TestClient
+from sqlalchemy import create_engine, event
+from sqlalchemy.orm import Session
 
 from ordo_fast.app import app
+from ordo_fast.models import table_regristy
 
 
 @pytest.fixture
 def client():
     return TestClient(app)
+
+
+@pytest.fixture
+def session():
+    engine = create_engine('sqlite:///:memory:')
+
+    table_regristy.metadata.create_all(engine)
+
+    with Session(engine) as session:
+        yield session
+
+    table_regristy.metadata.drop_all(engine)
+
+
+@contextmanager
+def _mock_db_time(*, model, time=datetime(2025, 5, 20)):
+    def fake_time_hook(mapper, connection, target):
+        if hasattr(target, 'created_at'):
+            target.created_at = time
+
+    event.listen(model, 'before_insert', fake_time_hook)
+
+    yield time
+
+    event.remove(model, 'before_insert', fake_time_hook)
+
+
+@pytest.fixture
+def mock_db_time():
+    return _mock_db_time
