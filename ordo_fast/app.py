d4398fa2f1283eed2416c02d5390b990210648fd
DevTarcizio <159829865+DevTarcizio@users.noreply.github.com>
2026-01-21 19:40:22 -0300

    WIP: Adding CORS

--- ordo_fast/app.py
+++ ordo_fast/app.py
@@ -1,12 +1,22 @@
 from http import HTTPStatus
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from ordo_fast.database import close_engine
 from ordo_fast.routers import auth, task, users
 from ordo_fast.schemas import Message
 
 app = FastAPI(title='Ordo Praesidium')
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(auth.router)
 app.include_router(users.router)
 app.include_router(task.router)
